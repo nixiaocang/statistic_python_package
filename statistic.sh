107cb55dbdff013e43afa8f38f3ec85ae06d790b
nixiaocang <470314062@qq.com>
2019-12-02 16:08:17 +0800

    Update statistic.sh
    优化统计脚本

--- statistic.sh
+++ statistic.sh
@@ -1,16 +1,12 @@
 #!/bin/env/bash
 find . -type f |grep py|xargs grep -E '^from|^import'| awk -F "py:" {'print $2'}|sort|uniq>res0.log
-sed -i 's/from/import/g' res0.log
-cat res0.log |awk -F "import " {'print $2'}|awk -F '.' {'print $1'}> res1.log
+sed 's/from/import/g' res0.log| awk -F "import " {'print $2'} |awk -F '.' {'print $1'}|sort|uniq > res1.log
 sed -i "s/,[ ]*/\n/g" res1.log
-sed -i “s/\s/\n/g" res1.log
-cat res1.log|sort|uniq |while read line
+sed -i "s/\s/\n/g" res1.log
+sed '/^$/d' res1.log|sort|uniq|while read line
 do
-    echo `pip list|grep $line` >> res2.log
+    echo `pip freeze|grep $line` >> res2.log
 done
-awk NF res2.log > res3.log
-sed -i 's/ (/==/g' res3.log
-sed -i 's/)//g' res3.log
-sed -i "s# #\n#g" res3.log
-sort res3.log|uniq >requests.txt
-rm /tmp/res*
+sed -i "s/\s/\n/g" res2.log
+sed '/^$/d' res2.log|sort|uniq > requirements.txt
+rm res*
